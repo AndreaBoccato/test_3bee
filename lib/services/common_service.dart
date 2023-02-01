9c8f305e45a4020403dec036d5136ec00ed8310b
AndreaBoccato <boccato9330@gmail.com>
2023-02-01 22:33:15 +0100

    Fix common service

--- lib/services/common_service.dart
+++ lib/services/common_service.dart
@@ -1,16 +1,22 @@
 import 'package:dio/dio.dart';
 
+///
+/// Essendoci solamente due chiamate api, per comodità racchiudo
+/// tutto in un unico file
+///
 class CommonService {
-  final String _baseUrl = 'https://api.3bee.com/api/v1';
   late Dio dio;
 
   CommonService() {
-    dio = Dio();
+    final BaseOptions options = BaseOptions(
+      baseUrl: 'https://api.3bee.com/api/v1',
+    );
+    dio = Dio(options);
   }
 
   Future authenticateUser({required String email, required String password}) async {
     final response = await dio.post(
-      '$_baseUrl/auth/jwt/create',
+      '/auth/jwt/create',
       data: {
         'email': email,
         'password': password,
